9a8961f004d19213074bb538d01775d4016400a3
sakshamrahate420-ctrl <sakshamrahate420@gmail.com>
2026-07-20 18:37:17 +0530

    Add files via upload

---
+++ python/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+if GROQ_API_KEY is None:
+    raise ValueError("GROQ_API_KEY not found. Did you create a .env file?")
