b002ded461a851c17a0e876a5e90238be374c41f
Saksham Rahate <sakshamrahate420@gmail.com>
2026-07-20 18:09:12 +0530

    Clean up build files and add project code

--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_REAL_FILE E:/GUI/REAL/build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/REAL.exe)
-set(__QT_DEPLOY_TARGET_REAL_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_REAL_RUNTIME_DLLS D:/QFTS/6.11.1/mingw_64/bin/Qt6Widgets.dll;D:/QFTS/6.11.1/mingw_64/bin/Qt6Gui.dll;D:/QFTS/6.11.1/mingw_64/bin/Qt6Core.dll)
--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/CMakeFiles/REAL.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for REAL.
-# This may be replaced when dependencies are built.
