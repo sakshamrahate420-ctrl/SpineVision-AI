ce23b76a6fd4a63ea43c3d56aeb19768065c7ca3
sakshamrahate420-ctrl <sakshamrahate420@gmail.com>
2026-07-21 09:49:13 +0530

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,25 +1,29 @@
 import sys
 from ai_client import generate_report
 
+
 def main():
     args = sys.argv
+
     if len(args) < 3:
         print("Error: Expected 2 arguments - MRI path and clinical notes")
         sys.exit(1)
+
     mri_path = args[1]
     clinical_notes = args[2]
+
     print("MRI Path:", mri_path)
     print("Clinical Notes:", clinical_notes)
 
-  
-   try:
-    report = generate_report(mri_path, clinical_notes)
-    print("AI Report:\n", report)
+    try:
+        report = generate_report(mri_path, clinical_notes)
+        print("AI Report:\n", report)
 
-except Exception as e:
-    print("========== PYTHON ERROR ==========")
-    print(type(e).__name__)
-    print(str(e))
+    except Exception as e:
+        print("========== PYTHON ERROR ==========")
+        print(type(e).__name__)
+        print(str(e))
+
 
 if __name__ == "__main__":
     main()
